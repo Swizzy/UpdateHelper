ca7d9439fd846d8d3bf1901bbebe52f16d121fce
Swizzy <swizzy@xeupd.com>
2015-09-11 11:00:33 +0200

    Initial Code Commit

---
+++ AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UpdateHelper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UpdateHelper")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e818ce6c-3f37-4dee-a910-2aa8fe96ad2a")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace UpdateHelper {
+
+    internal static class Program {
+        private static void Main(string[] args) {
+            try {
+                Thread.Sleep(1000);
+                if(args.Length != 3)
+                    Usage();
+                if(!IsNumeric(args[0]))
+                    KillApp(args[0]);
+                else
+                    KillApp(int.Parse(args[0]));
+                if(!File.Exists(args[1]))
+                    Usage();
+                else if(File.Exists(args[2]))
+                    File.Delete(args[2]);
+                File.Move(args[1], args[2]);
+                Process.Start(args[2]);
+                Environment.Exit(0);
+            }
+            catch(Exception ex) {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private static void Usage() {
+            MessageBox.Show(string.Format("Usage: {0} [ProcessName/PID] [New Executable Path] [Old Executable Path]", Path.GetFileName(Application.ExecutablePath)), "Bad arguments to Swizzy's Update helper!", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            Environment.Exit(1);
+        }
+
+        private static bool IsNumeric(string input) {
+            int result;
+            return int.TryParse(input, out result);
+        }
+
+        private static void KillApp(string appname) {
+            try {
+                var processesByName = Process.GetProcessesByName(appname);
+                if(processesByName.Length <= 0)
+                    return;
+                foreach(var process in processesByName) {
+                    if(process.HasExited)
+                        continue;
+                    process.Kill();
+                    process.WaitForExit();
+                }
+                KillApp(appname);
+            }
+            catch(Exception ex) { }
+        }
+
+        private static void KillApp(int id) {
+            try {
+                var processById = Process.GetProcessById(id);
+                if(processById.HasExited)
+                    return;
+                processById.Kill();
+                processById.WaitForExit();
+            }
+            catch(Exception ex) { }
+        }
+    }
+
+}
